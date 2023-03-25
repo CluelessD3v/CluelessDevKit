1de95496ab4680a28df419187d922a27f4c540e8
CluelessD3v <cluelessdevmail@gmail.com>
2023-03-25 02:52:51 -0500

    Delete most of wally packages refs and updated utilities

--- src/Shared/Data/Types.lua
+++
@@ -1,7 +0,0 @@
-local Types = {}
-export type Array<T> = {number: T}
-export type Map<K, T> = {K: T}
-export type Matrix<T> = {number:{number: T}}
-
-
-return Types
--- src/Shared/Utilities/Functions/WeightedChoice.lua
+++ src/Shared/Utilities/Functions/WeightedChoice.lua
@@ -1,14 +1,14 @@
-return function(Example)
+return function(weights: {[string]: number})
      -- Gets max probability
      local Total = 0
-     for _, Probability in pairs(Example) do
+     for _, Probability in pairs(weights) do
          Total += Probability
      end
      
      local Random = math.random(Total)
      local Sum = 0
 
-     for Name,Probability in pairs(Example) do
+     for Name,Probability in pairs(weights) do
          Sum += Probability
          if Random <= Sum then
              return Name
