ae4b80967d1398104d41a70cc5fe9b0ec0bd44e5
CluelessD3v <cluelessdevmail@gmail.com>
2023-06-28 16:25:17 -0500

    Mod ReadMe

--- src/Shared/DevKit/Functions/Color3Lerp.lua
+++ src/Shared/DevKit/Functions/Color3Lerp.lua
@@ -2,7 +2,9 @@
     Small Color lerping libary, all credit to Alan Zucconi, this code was ripped straight
     from his blogpost here https://www.alanzucconi.com/2016/01/06/colour-interpolation/
 
-    Note that this is an extremely simple color lerp doesn't yield good results, use 
+    Note that this is an extremely simple color lerp doesn't yield good results, 
+    use it only if you need a quick gradient, else this plug-in can actual number
+    Sequences to make HCL gradients: https://devforum.roblox.com/t/cse-the-ultimate-gradient-editor/2171637
 ]]
 
 local LerpRGB = function(a: Color3, b: Color3, t: number)
