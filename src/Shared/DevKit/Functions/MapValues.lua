2351296be611477cadae688fd881826497bcd243
CluelessD3v <cluelessdevmail@gmail.com>
2023-06-28 12:40:30 -0500

    refactor MapValues

--- src/Shared/DevKit/Functions/MapValues.lua
+++ src/Shared/DevKit/Functions/MapValues.lua
@@ -1,4 +1,6 @@
-return function (x: number, min: number, max: number, minScale, maxScale: number): number
-     x = (x - min)/(max - min) * maxScale
-     return math.clamp(x, minScale, maxScale)
- end
+local function MapValues(x: number, min: number, max: number, minScale, maxScale: number): number
+	x = (x - min) / (max - min) * maxScale
+	return math.clamp(x, minScale, maxScale)
+end
+
+return MapValues
