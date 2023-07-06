5e7513d18d2c11357a3b4ebe44064a49ce6ca7b9
CluelessD3v <cluelessdevmail@gmail.com>
2023-07-06 01:05:28 -0500

    vertical allign

--- src/Shared/DevKit/Libraries/RaycastFromMouse.lua
+++ src/Shared/DevKit/Libraries/RaycastFromMouse.lua
@@ -1,11 +1,12 @@
+--stylua: ignore start
 local UserInputService = game:GetService("UserInputService")
 local Camera = workspace.CurrentCamera
 
 local RaycastFromMouse = {}
 
 RaycastFromMouse.FromViewportPoint = function(distance, raycastParams): RaycastResult
-	local mousePos = UserInputService:GetMouseLocation()
-	local unitRay: Ray = Camera:ViewportPointToRay(mousePos.X, mousePos.Y)
+	local mousePos      = UserInputService:GetMouseLocation()
+	local unitRay: Ray  = Camera:ViewportPointToRay(mousePos.X, mousePos.Y)
 	local rayCastResult = workspace:Raycast(unitRay.Origin, unitRay.Direction * distance, raycastParams)
 	return rayCastResult
 end
@@ -14,10 +15,11 @@ end
 -- always be 32 pixels lower than where your mouse actually is
 
 RaycastFromMouse.FromScreenPoint = function(distance, raycastParams): RaycastResult
-	local mousePos = UserInputService:GetMouseLocation()
-	local unitRay: Ray = Camera:ScreenPointToRay(mousePos.X, mousePos.Y)
+	local mousePos      = UserInputService:GetMouseLocation()
+	local unitRay: Ray  = Camera:ScreenPointToRay(mousePos.X, mousePos.Y)
 	local rayCastResult = workspace:Raycast(unitRay.Origin, unitRay.Direction * distance, raycastParams)
 	return rayCastResult
 end
 
 return RaycastFromMouse
+--stylua: ignore end
