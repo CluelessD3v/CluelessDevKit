86fbd59b64fb7527f034847472e8e2bd7bc9adfb
CluelessD3v <cluelessdevmail@gmail.com>
2023-07-06 20:39:30 -0500

    Update V3ToV3I16

--- src/Shared/DevKit/Functions/V3ToV3i16.lua
+++ src/Shared/DevKit/Functions/V3ToV3i16.lua
@@ -1,7 +1,9 @@
-return function(v: Vector3)
-    if typeof(v) ~= "Vector3" then
-        error(v.." is not a vector")
-    end
+local function V3ToV3i16(v: Vector3)
+	if typeof(v) ~= "Vector3" then
+		error(v .. " is not a vector")
+	end
 
-    return Vector3int16.new(v.X, v.Y, v.Z)   
+	return Vector3int16.new(v.X, v.Y, v.Z)
 end
+
+return Vector3int16
