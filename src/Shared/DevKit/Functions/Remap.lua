59e60dc848ebd1d4f65008b9540f8bcdd8782aa0
CluelessD3v <cluelessdevmail@gmail.com>
2023-10-22 01:20:31 -0500

    rewrote arg names and commets

--- src/Shared/DevKit/Functions/Remap.lua
+++ src/Shared/DevKit/Functions/Remap.lua
@@ -1,19 +1,19 @@
 --[=[
-	This is a more nuanced form of value normalization that allows me to map and 
-	normalize values to a desired scale range, ensuring they stay within the 
-	specified bounds.
+	This is a more nuanced form of value normalization that allows me to map 
+	values to a desired scale range, ensuring they stay within the 	specified 
+	bounds.
 
 	i.e let's say I want to convert player health from a [0, 100] range to a 
 	[0, 1] range!
 
 	given a current health of 25 this function wold return
 
- 	ScaleValueToRange(25, 0, 100, 0, 1) --> 0.25
+ 	Remap(25, 0, 100, 0, 1) --> 0.25
 ]=]
 
-local function Remap(x: number, min: number, max: number, minScale, maxScale: number): number
-	x = (x - min) / (max - min) * maxScale
-	return math.clamp(x, minScale, maxScale)
+local function Remap(x: number, inputMin: number, inputMax: number, outputMin, outputMax: number): number
+	x = (x - inputMin) / (inputMax - inputMin) * outputMax
+	return math.clamp(x, outputMin, outputMax)
 end
 
 return Remap
